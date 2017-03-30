cba6ca184aa5d9600abbd1a8a6f7d949487cc465
Veronika Ivanova <veronikaiva95@gmail.com>
2017-03-30 22:08:20 +0300

    Calc was created

---
+++ CalcTest/CalcTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Patterns_z7;
+
+namespace CalcTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        Calculator calculator = new Calculator();
+       
+        [TestMethod]
+        public void TestPlus()
+        {
+            int res = calculator.Plus(10, 10);
+            Assert.AreEqual( 20,res);
+        }
+
+        [TestMethod]
+        public void TestMinus()
+        {
+            int res = calculator.Minus(10, 5);
+            Assert.AreEqual(5,res);
+        }
+
+        [TestMethod]
+        public void TestMult()
+        {
+
+           
+            int res = calculator.Mult(10, 10);
+            Assert.AreEqual( 100,res);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDevWithZeroEx()
+        {
+            int res = calculator.Dev(10, 0);
+            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestSqrtWithE()
+        {
+            double res = calculator.Sqrt(-10);
+
+        }
+        [TestMethod]
+        public void TestSqrt()
+        {
+            double res = calculator.Sqrt(36);
+            Assert.AreEqual(6, res);
+        }
+
+
+        [TestMethod]
+        public void TestDev()
+        {
+            int res = calculator.Dev(10, 2);
+            Assert.AreEqual(5,res);
+        }
+    }
+}
---
+++ Patterns_z7/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns_z7
+{
+  public   class Calculator
+    {
+        public int  Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dev(int a, int b)
+        { 
+        
+            return a / b;
+        }
+        public double Sqrt(int a)
+        {
+            if (a < 0) throw new Exception();
+            return Math.Sqrt(a);
+        }
+    }
+}
